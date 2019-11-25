4de03628ce26f0ae5fa1ffea5edcf3dbeeeded27
Calvin Figuereo-Supraner <mail@calvin.page>
2019-11-25 15:38:15 -0800

    Change heuristic to plaintext on 3

--- test_c03.py
+++ test_c03.py
@@ -6,18 +6,18 @@ import Cryptodome.Util.strxor as strxor
 import lib.ciphers as ciphers
 
 
-def find_key(ct: bytes) -> int:
+def heuristic(pt: bytes) -> int:
     # We can get away with a cheap heuristic because the cipher is simple.
-    def heuristic(k: int) -> int:
-        return strxor.strxor_c(ct, k).count(b" ")
+    return pt.count(b" ")
 
-    return max(range(256), key=heuristic)
+
+def find_pt(ct: bytes) -> bytes:
+    pts = (strxor.strxor_c(ct, k) for k in range(256))
+    return max(pts, key=heuristic)
 
 
 def test_solution():
     ct = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
     pt = b"Cooking MC's like a pound of bacon"
 
-    key = find_key(bytes.fromhex(ct))
-    pt_found = ciphers.SingleXorCipher(key).decrypt(bytes.fromhex(ct))
-    assert pt_found == pt
+    assert find_pt(bytes.fromhex(ct)) == pt
