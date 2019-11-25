639f224c844c45da247c0d4143db36c9dd0f7588
Calvin Figuereo-Supraner <mail@calvin.page>
2019-11-24 20:41:36 -0800

    Pull heuristic into outer scope

--- test_c04.py
+++ test_c04.py
@@ -3,15 +3,16 @@ from typing import List
 import Cryptodome.Util.strxor as strxor
 
 
-def find_pt(cts: List[bytes]) -> bytes:
-    def heuristic(pt: bytes) -> int:
-        # 1 point for every character in " etaoin"; higher is better.
-        score, good = 0, set(b" etaoin")
-        for b in pt:
-            if b in good:
-                score += 1
-        return score
+def heuristic(pt: bytes) -> int:
+    # 1 point for every character in " etaoin"; higher is better.
+    score, good = 0, set(b" etaoin")
+    for b in pt:
+        if b in good:
+            score += 1
+    return score
 
+
+def find_pt(cts: List[bytes]) -> bytes:
     pts = (strxor.strxor_c(ct, k) for ct in cts for k in range(256))
     return max(pts, key=heuristic)
 
