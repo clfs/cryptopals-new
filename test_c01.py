ac6fb9f93468bb8e672e6516221e017ca419ab7b
Calvin Figuereo-Supraner <mail@calvin.page>
2019-11-24 19:31:17 -0800

    Fix newlines

--- test_c01.py
+++ test_c01.py
@@ -7,4 +7,5 @@ import lib.encoding as encoding
 def test_solution():
     a = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     b = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    
     assert encoding.hex_to_base64(a) == b
