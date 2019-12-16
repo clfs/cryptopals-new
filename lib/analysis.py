32f44b9d807a4e8fa0c11b092712e3b53cea004d
Calvin Figuereo-Supraner <mail@calvin.page>
2019-12-15 20:30:06 -0800

    Fix types in 8

--- lib/analysis.py
+++ lib/analysis.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
 from Cryptodome.Cipher import AES
 
 import lib.iteration as iteration
 
 
-def is_aes_ecb_ct(x: bytes) -> bool:
+def is_aes_ecb_ct(x: Optional[bytes]) -> bool:
+    if x is None:
+        return False
     blocks = iteration.blocks(x, AES.block_size)
     return len(blocks) > len(set(blocks))  # True if repeated block
