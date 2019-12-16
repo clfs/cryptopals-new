6fd5026b8a795fdbeb42aaac38d6f6e5a0e50ccf
Calvin Figuereo-Supraner <mail@calvin.page>
2019-12-15 20:50:52 -0800

    Fix types in 13

--- test_c13.py
+++ test_c13.py
@@ -13,8 +13,8 @@ def create_admin_profile(manager: services.EcbProfileManager) -> bytes:
     # This isn't really a common scenario, but it saves a lot of boilerplate
     # around brute-forcing block sizes and offsets. I think this is what the
     # authors probably intended.
-    magic_block = Padding.pad(b"admin", AES.block_size).decode()
-    email1 = "A" * (AES.block_size - len("email=")) + magic_block
+    magic_block = Padding.pad(b"admin", AES.block_size)
+    email1 = "A" * (AES.block_size - len("email=")) + magic_block.decode()
     email2 = "A" * (2 * AES.block_size - len("email=") - len("&uid=10&role="))
     profile1 = manager.profile_for(email1)
     profile2 = manager.profile_for(email2)
