3731d78e16e1b09a4816308d5d7ff0d7e001258c
Calvin Figuereo-Supraner <mail@calvin.page>
2019-12-15 20:35:33 -0800

    List to Iterator on 4

--- test_c04.py
+++ test_c04.py
@@ -1,20 +1,19 @@
 # 4. Detect single-character XOR
-from typing import List
+from typing import Iterator
 
 import Cryptodome.Util.strxor as strxor
 
 
 def heuristic(pt: bytes) -> int:
-    # Cheaper heuristic.
     return pt.count(b" ") + pt.count(b"e")
 
 
-def find_pt(cts: List[bytes]) -> bytes:
+def find_pt(cts: Iterator[bytes]) -> bytes:
     pts = (strxor.strxor_c(ct, k) for ct in cts for k in range(256))
     return max(pts, key=heuristic)
 
 
 def test_solution() -> None:
     with open("data/04.txt") as f:
-        cts = [bytes.fromhex(line) for line in f]
-    assert find_pt(cts) == b"Now that the party is jumping\n"
+        cts = (bytes.fromhex(line) for line in f)
+        assert find_pt(cts) == b"Now that the party is jumping\n"
